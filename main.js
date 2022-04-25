d2a9931a597854df6f9de7c689ec2059cdb42767
ak2296 <mohamad.gholinia@gmail.com>
2022-04-25 21:13:49 +0200

    fixed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    getLedamot();
+
+	getLedamot();
     function getLedamot(){
         let url ="http://127.0.0.1:3000/api/getpolitician"
         $.ajax({
@@ -8,62 +9,23 @@ $(document).ready(function() {
             type:"JSON"
         }).done(function(data){
             let ledamot= data.personlista.person;
-			let personout='';
-            let allparti= []
             let output= '';
 
-            $.each(ledamot, function(i, person){
-				if(!allparti.includes(person.parti)){
-                allparti.push(person.parti)}});
+			$.each(ledamot, function(i, person){
 
-				personout +=`
-				
-				`
-
-			$.each(allparti, function(i,parti){
-
-            output +=`
-			
-			<h2 class="accordion-header" id="accordionhead">
-
-            	<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}" id="partinamn">
-            	<strong>${parti}</strong>
-            	</button>
-				</h2>
-		<div id="collapse${i}" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-		  <table class="table">
-			  <thead>
-				<tr>
-				  <th scope="col">Index</th>
-				  <th scope="col">Namn</th>
-				</tr>
-			  </thead>
-			  <tbody>
-				<tr>
-				  <th scope="row" id="">1</th>
-				  <td>example1</td>${
-                $.each(ledamot, function (i,person){
-                    if(person.parti===parti){
-                        console.log(person.tilltalsnamn)
-                    }
-                })}
-				</tr>
-				<tr>
-				  <th scope="row" id="">2</th>
-				  <td>example2</td>
-				</tr>
-				<tr>
-				  <th scope="row" id="">3</th>
-				  <td >example3</td>
-				</tr>
-			  </tbody>
-			</table>
-		</div>
+				output +=`
+			<tr>
+			<th scope="row" class='col1'>${i}</th>
+			<td class='col2'><a class='text-decoration-none link-dark' href="#">${person.tilltalsnamn}</a></td>
+			<td class='col3'><a class='text-decoration-none link-dark' href="#">${person.efternamn}</a></td>
+			<td class='col4'><a >${person.parti +'  '}</a> </td>
+			</tr>
                `;
-            })
-            $('#accordionItem').html(output);
+			})
+
+		$('#memberTable').html(output);
         }).fail(function(data){
             console.log("Error");
         });
-    }
+	}
 });
