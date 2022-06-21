b5b8797d1d89554415603efbae7b14b117b11ddb
Alexandra Koch <alexandra@alexandrakoch.se>
2022-06-21 16:25:30 +0200

    Merge branch 'main' of github.com:twgust/politiknorden

--- twitter.js
+++ twitter.js
@@ -3,6 +3,12 @@ module.exports = {
         var tweets = [];
         let count = responseBody['meta']['result_count'].valueOf();
 
+        // no results, return.
+        if (count === 0){
+            return;
+        }
+
+        // else, process the response.
         for (let i = 0; i < count; i++) {
             let created_id = responseBody['data'][i]['created_id'];
             let tweetText = responseBody['data'][i]['text'];
@@ -11,13 +17,15 @@ module.exports = {
             let userID = responseBody['data'][i]['author_id'].valueOf();
             let authorID = responseBody['includes']['users'][i]['id'].valueOf();
 
-
+            // by joining userID on authorID we can get the username
+            // since userID and authorID represent the same value
             if(userID === authorID){
                     var userName = responseBody['includes']['users'][i]['username'];
                     console.log('Date: ' + created_id);
                     console.log('Author: ' + userName);
                     console.log('Text: ' + tweetText);
                     console.log('id: ' + tweetID);
+                    // add a tweet object to the Json array
                     tweets[i] = {
                         date: created_id,
                         author: userName,
@@ -25,8 +33,8 @@ module.exports = {
                         id: tweetID
                     }
                 }
-                //console.log(responseBody['data'][i]['text']);
-                console.log('\n\n')
         }
+        // return processed response.
+        return tweets;
     }
 }
