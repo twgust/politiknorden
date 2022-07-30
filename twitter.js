b2a4245605f620f770a075a7b373811aa4fe1482
Alexandra Koch <alexandra@alexandrakoch.se>
2022-07-30 19:20:57 +0200

    Changed twitter.js for implementation in app
    Changed twitter.js to add a meta object with a results_count object inside with a value equivalent to the number of tweets in the object.

--- twitter.js
+++ twitter.js
@@ -1,42 +1,50 @@
 module.exports = {
     processTwitterResponse: function(responseBody){
-        var tweets = [];
+        var tweets = {};
         let count = responseBody['meta']['result_count'].valueOf();
         console.log(responseBody)
 
         // no results, return.
         if (count === 0){
-            return;
+            tweets.meta = {
+                result_count: count
+            }
+            return tweets;
         }
 
-        // else, process the response.
+        else {
         for (let i = 0; i < count; i++) {
-            let created_id = responseBody['data'][i]['created_at'];
-            let tweetText = responseBody['data'][i]['text'];
-            let tweetID = responseBody['data'][i]['id'];
+                let created_id = responseBody['data'][i]['created_at'];
+                let tweetText = responseBody['data'][i]['text'];
+                let tweetID = responseBody['data'][i]['id'];
 
-            let userID = responseBody['data'][i]['author_id'].valueOf();
-            for (let j = 0; j < responseBody['includes']['users'].length; j++) {
-                let authorID = responseBody['includes']['users'][j]['id'].valueOf();
-                    // by joining userID on authorID we can get the username
-                    // since userID and authorID represent the same value
-                if(userID === authorID){
-                var userName = responseBody['includes']['users'][j]['username'];
-                console.log('Date: ' + created_id);
-                console.log('Author: ' + userName);
-                console.log('Text: ' + tweetText);
-                console.log('id: ' + tweetID);
-                // add a tweet object to the Json array
-                    tweets[i] = {
-                        date: created_id,
-                        author: userName,
-                        text: tweetText,
-                        id: tweetID}
-                    console.log("\n\n");
+                let userID = responseBody['data'][i]['author_id'].valueOf();
+                for (let j = 0; j < responseBody['includes']['users'].length; j++) {
+                    let authorID = responseBody['includes']['users'][j]['id'].valueOf();
+                        // by joining userID on authorID we can get the username
+                        // since userID and authorID represent the same value
+                    if(userID === authorID){
+                    var userName = responseBody['includes']['users'][j]['username'];
+                    console.log('Date: ' + created_id);
+                    console.log('Author: ' + userName);
+                    console.log('Text: ' + tweetText);
+                    console.log('id: ' + tweetID);
+                    // add a tweet object to the Json array
+                        tweets[i] = {
+                            date: created_id,
+                            author: userName,
+                            text: tweetText,
+                            id: tweetID}
+                        console.log("\n\n");
+
+                    }
                 }
             }
         }
         // return processed response.
+        tweets.meta = {
+            result_count: count
+        }
         return tweets;
     }
 }
